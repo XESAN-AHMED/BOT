c31d2426bf3ae7d796f5d1beb2cec5a47c1b823d
XESAN AHMED <165814053+XESAN-AHMED@users.noreply.github.com>
2024-04-03 08:51:15 +0600

    Update fuck.js

--- scripts/commands/fuck.js
+++ scripts/commands/fuck.js
@@ -70,6 +70,6 @@ module.exports.run = async function ({ event, api, args }) {
   if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
   else {
       const one = senderID, two = mention[0];
-      return makeImage({ one, two }).then(path => api.sendMessage({ body: "", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+      return makeImage({ one, two }).then(path => api.sendMessage({ body: "𝗙𝘂𝗖𝗸 𝘆𝗢𝘂 𝗕𝗶𝘁𝗖𝗵🐸🔞", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
   }
     }
